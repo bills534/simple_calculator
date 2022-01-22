67c8aea9d80d3181e7357c6d22aded2e3adb3001
Bill <bills534@gmail.com>
2022-01-22 10:34:57 -0500

    entering numbers and operations working

--- script.js
+++ script.js
@@ -21,10 +21,29 @@ class Calculator {
     }
 
     appendNumber(number){
-        this.currentOperand = number;
+        if(number === '.' && this.currentOperand.includes('.')) {
+            // skipping . if one already is in number
+            return;
+        }
+        this.currentOperand = this.currentOperand.toString() + number.toString();
     }
 
     chooseOperation(operation){
+        this.operation = operation;
+        
+        // ignore empty/invalid operations
+        if(this.currentOperand === '' || this.currentOperand === '.') {
+            return;
+        }
+
+        // if there is math to do, do it
+        if(this.previousOperand !== '') {
+            this.compute();
+        }
+        
+        // setting previous line to current line when operation entered
+        this.previousOperand = `${this.currentOperand} ${operation}`;
+        this.currentOperand = '';
 
     }
 
@@ -33,12 +52,14 @@ class Calculator {
     }
 
     updateDisplay(){
+        // setting current operation div = current operand
         this.currentOperandTextElement.innerText = this.currentOperand;
+        this.previousOperandTextElement.innerText = this.previousOperand;
     }
 }
 
 const numberButtons = document.querySelectorAll('[data-number]');
-const operationButtons = document.querySelectorAll('[data-operations]');
+const operationButtons = document.querySelectorAll('[data-operation]');
 const equalsButton = document.querySelector('[data-equals]');
 const deleteButton = document.querySelector('[data-delete]');
 const allClearButton = document.querySelector('[data-all-clear]');
@@ -51,6 +72,13 @@ const calculator = new Calculator(previousOperandTextElement, currentOperandText
 numberButtons.forEach(button =>{
     button.addEventListener('click', ()=>{
         calculator.appendNumber(button.innerText);
+        calculator.updateDisplay();
+    });
+});
+
+operationButtons.forEach(button =>{
+    button.addEventListener('click', ()=>{
+        calculator.chooseOperation(button.innerText);
         calculator.updateDisplay();
     });
 });
